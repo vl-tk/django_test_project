6d805b24ac95818f944dde03d7a5b1bb11463c48
worker <worker@work-pc>
2021-04-12 18:52:41 +0200

    Add check_code command test

--- codes_generator/tests.py
+++ codes_generator/tests.py
@@ -10,9 +10,11 @@ from django.test import TestCase
 
 class GenerateCodeTest(TestCase):
     def setUp(self):
-        codes_file = settings.BASE_DIR.joinpath("codes_generator/results/test.json")
-        if codes_file.exists():
-            codes_file.unlink()
+        self.codes_file = settings.BASE_DIR.joinpath(
+            "codes_generator/results/test.json"
+        )
+        if self.codes_file.exists():
+            self.codes_file.unlink()
 
     def test_generate_code_command_result(self):
         out = StringIO()
@@ -48,10 +50,9 @@ class GenerateCodeTest(TestCase):
 
         # проверяем результат на соответствие
 
-        codes_file = settings.BASE_DIR.joinpath("codes_generator/results/test.json")
-        self.assertTrue(codes_file.exists())
+        self.assertTrue(self.codes_file.exists())
 
-        with open(codes_file, "r") as f:
+        with open(self.codes_file, "r") as f:
             codes = json.load(f)
 
         self.assertIn("агенства", codes)
@@ -62,6 +63,49 @@ class GenerateCodeTest(TestCase):
         self.assertEqual(len(codes["1"]), 5)
 
     def tearDown(self):
-        codes_file = settings.BASE_DIR.joinpath("codes_generator/results/test.json")
-        if codes_file.exists():
-            codes_file.unlink()
+        if self.codes_file.exists():
+            self.codes_file.unlink()
+
+
+class CheckCodeTest(TestCase):
+    def setUp(self):
+
+        self.codes_file = settings.BASE_DIR.joinpath(
+            "codes_generator/results/test.json"
+        )
+
+        data = {
+            "my_group": [
+                "Thqty7vVTD",
+                "hibaY7YeAB",
+                "lWtO61P4FF",
+                "qAiN0SK9St",
+                "PqRStDUFKi",
+                "QTZRJg5nTJ",
+            ]
+        }
+
+        with open(self.codes_file, "w") as f:
+            json.dump(data, f)
+
+    def test_check_code_command_result(self):
+        out = StringIO()
+
+        call_command(
+            "check_code",
+            "lWtO61P4FF",
+            "--filename=test.json",
+            stdout=out,
+        )
+        self.assertIn("found for group", out.getvalue())
+
+        call_command(
+            "check_code",
+            "123456",
+            "--filename=test.json",
+            stdout=out,
+        )
+        self.assertIn("NOT found", out.getvalue())
+
+    def tearDown(self):
+        self.codes_file.unlink()
