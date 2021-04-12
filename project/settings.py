58be925f53ec1f8025578d130a2504909c52852e
worker <worker@work-pc>
2021-04-12 17:26:31 +0200

    Fix secret key formatting

--- project/settings.py
+++ project/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
-    "django-insecure-knt=oa_!&7!65tpd2$i+lx(q45%a4h-&*t(%wsdwg@7nx10*^4"
+SECRET_KEY = "django-insecure-knt=oa_!&7!65tpd2$i+lx(q45%a4h-&*t(%wsdwg@7nx10*^4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
